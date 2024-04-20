67fcb55598fbf20086a96705d53a3247aad25f34
Zaman00007 <shahidzaman75724@gmail.com>
2024-04-20 22:44:13 +0530

    Graph

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,7 @@
 class AdminController < ApplicationController
     before_action :authenticate_admin!
     def index
-        @registration_logs = PatientRegistrationLog.group(:date).order(date: :asc).count
+      @registration_logs = PatientRegistrationLog.all
     end
-end
+  end
+  
